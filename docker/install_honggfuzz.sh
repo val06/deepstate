2e91f56f24474c294b7a04e250dd4fb965ea942b
val06 <valeri.pliskin@gmail.com>
2020-03-22 10:30:13 +0200

    Update install_honggfuzz.sh

--- docker/install_honggfuzz.sh
+++ docker/install_honggfuzz.sh
@@ -2,11 +2,11 @@
 set -e
 
 # Install dependencies
-sudo apt-get install -y binutils-dev \
+sudo apt-get update && sudo apt-get install -y binutils-dev \
     libunwind-dev \
     && sudo rm -rf /var/lib/apt/lists/*
 
 # Install Honggfuzz
 git clone https://github.com/google/honggfuzz \
     && cd honggfuzz \
-    && make -j $1
+    && make -j1
